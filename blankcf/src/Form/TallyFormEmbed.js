67408a83df93f250010e7a248b03b1c607754977
Kidus Sebsibe <sonofmulugeta@gmail.com>
2024-04-07 05:08:25 -0400

    Added new form and emptied bloated dropdown menu

--- blankcf/src/Form/TallyFormEmbed.js
+++ blankcf/src/Form/TallyFormEmbed.js
@@ -10,13 +10,13 @@ const TallyFormEmbed = () => {
       
       {
       <iframe
-        data-tally-src="https://tally.so/r/mJ6jQr"
+        data-tally-src="https://tally.so/r/nr68Ov"
         width="100%"
         height="100%"
         frameBorder="0"
         marginHeight="0"
         marginWidth="0"
-        title="Career form"
+        title="Pivot form"
         style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
       ></iframe>
 
